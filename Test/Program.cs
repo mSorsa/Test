653538519b6218e90a3550a2d8b5338bbb0e215a
mSorsa <martin.sorsa@gmail.com>
2022-11-10 05:27:54 +0100

    added comments in program.cs

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,17 +1,22 @@
 ﻿using Test.SearchAlgos;
 using Test.SortingAlgos;
 
+// An array with random integers
 int[] unsortedArray = new int[] { 5, 10, 2, 3, 25, 18, 19, 20, 21, 11, 1, 6, 7, 16, 17, 12, 13, 14, 15, 23, 24, 8, 9, 4, 22 };
-int valueToFind = 7;
+int valueToFind = 7;    // The value to find in the array
 
-var sortedArray = Quick.QuickSortAlgorithm(unsortedArray, 0, unsortedArray.Length - 1);
+// Save original position of the value to find
 int itemOriginalPosition = Array.IndexOf(unsortedArray, valueToFind);
+// Sort the array
+var sortedArray = Quick.QuickSortAlgorithm(unsortedArray, 0, unsortedArray.Length - 1);
 
+// Print the sorted array
 sortedArray.ToList().ForEach(x => Console.WriteLine(x));
 Console.WriteLine();
 
-
+// Outputs:
 Console.WriteLine($"Item with value {valueToFind} is in position: {Binary.Search(arr:sortedArray, target:valueToFind)}");
 Console.WriteLine($"Items original position was: {itemOriginalPosition}");
 
+// Hold console window open.
 Console.ReadKey();
