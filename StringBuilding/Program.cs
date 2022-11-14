f7f1b1ab6c51d70cf5f46dca6e46339194d5681b
mSorsa <martin.sorsa@gmail.com>
2022-11-14 05:04:39 +0100

    More cleaning in StringBuilding programcs

--- StringBuilding/Program.cs
+++ StringBuilding/Program.cs
@@ -2,8 +2,9 @@
 bool bt = true;
 while (bt)
 { 
-    Console.WriteLine("Number of iterations to try: ");
+    Console.Write("Number of iterations to try: ");
     string input = Console.ReadLine();
+    Console.WriteLine();
 
     int iterations = 0;
     if (int.TryParse(input, out iterations))
@@ -11,12 +12,12 @@ while (bt)
         // String builder speed (str.Append())
         var sbSpeedChecker = new SBSpeedChecker();
         double sbSpeed = sbSpeedChecker.SBAppendSpeed(iterations);
-        Console.WriteLine("----------------------------------------------------------");
         Console.WriteLine($"StringBuilder took: {sbSpeed} ms to append {iterations} things.");
         // Normal string speed
         double normalSpeed = sbSpeedChecker.NormalStringAppendSpeed(iterations);
         Console.WriteLine($"Normal string took: {normalSpeed} ms to append {iterations} things.");
         Console.WriteLine("----------------------------------------------------------");
+        Console.WriteLine();
     }
     else
     {
