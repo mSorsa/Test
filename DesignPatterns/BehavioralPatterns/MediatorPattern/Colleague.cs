c0c31b8f8c6f9837811048bd935a88bf35603d3d
mSorsa <martin.sorsa@gmail.com>
2023-01-08 13:10:55 +0100

    Removed unecessary usings in DesignPatterns-project.

--- DesignPatterns/BehavioralPatterns/MediatorPattern/Colleague.cs
+++ DesignPatterns/BehavioralPatterns/MediatorPattern/Colleague.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DesignPatterns.BehavioralPatterns.MediatorPattern
+﻿namespace DesignPatterns.BehavioralPatterns.MediatorPattern
 {
     public class Colleague : IColleague
     {
